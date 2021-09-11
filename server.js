a006550ea9a233f59d4111c64a471b201f421021
Myuze <Myuze@users.noreply.github.com>
2021-09-11 02:26:34 -0700

    Added nanoid package and DELETE method

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { nanoid } = require('nanoid');
+const fs = require('fs');
 const path = require('path');
 const db = require('./db/db');
 
@@ -7,15 +9,11 @@ const PORT = 3001;
 const app = express();
 
 app.use(express.json());
+app.use(express.static('public'));
 
-app.get('*', (req, res) => { 
-  if (res.statusCode === '404') return;
-  res.status(200).sendFile(path.join(__dirname, 'public/index.html'));
-});
 
 app.get('/notes', (req, res) => {
   if (res.statusCode === '404') return;
-  console.log(path.join(__dirname, 'public/notes.html'));
   res.status(200).sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
@@ -29,22 +27,47 @@ app.post('/api/notes', (req, res) => {
 
   if (title) {
     const newNote = {
+      id: nanoid(),
       title,
       text
     };
 
+    db.push(newNote);
+    console.log(db)
+    updateDb(db);
+    
     const response = {
       status: 'success',
       body: newNote
     };
-
+    
     console.log(response);
     res.status(201).json(response);
   } else {
     res.status(500).json('Error in posting note');
-  }
+  };
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  console.log(req.params.id)
+  console.log('BALETED')
+  console.log(db.indexOf(req.params.id))
+  db.splice(db.indexOf(req.params.id));
+
+  res.status(200);
+});
+
+app.get('*', (req, res) => { 
+  if (res.statusCode === '404') return;
+  res.status(200).sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)
 );
+
+function updateDb(db) {
+  fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
+    err ? console.log(new Error) : console.log('db.json updated!');
+  });
+}
