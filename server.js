bbeca20add14df0054e5fa5102996c583e2f5b95
Myuze <Myuze@users.noreply.github.com>
2021-09-11 21:15:03 -0700

    Added Delete note by id, and comments

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
+// Global Requirements
 const express = require('express');
 const { nanoid } = require('nanoid');
 const fs = require('fs');
 const path = require('path');
 const db = require('./db/db');
 
-const PORT = 3001;
+// Default Port
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static('public'));
 
-
+// Notes Main Index Route
 app.get('/notes', (req, res) => {
   if (res.statusCode === '404') return;
   res.status(200).sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
+// Notes API route
 app.get('/api/notes', (req, res) => {
   if (res.statusCode === '404') return;
   res.status(200).json(db);
 });
 
+// Add a new note using the post API route
 app.post('/api/notes', (req, res) => {
   const { title, text } = req.body;
 
@@ -33,7 +37,6 @@ app.post('/api/notes', (req, res) => {
     };
 
     db.push(newNote);
-    console.log(db)
     updateDb(db);
     
     const response = {
@@ -48,24 +51,30 @@ app.post('/api/notes', (req, res) => {
   };
 });
 
+// Delete note by Id route
 app.delete('/api/notes/:id', (req, res) => {
-  console.log(req.params.id)
-  console.log('BALETED')
-  console.log(db.indexOf(req.params.id))
-  db.splice(db.indexOf(req.params.id));
+  // Find index of object to be deleted by id
+  let index = db.findIndex(note => note.id === req.params.id);
 
-  res.status(200);
+  let deleted = db.splice(index, 1);
+  console.log(`${JSON.stringify(deleted)} DELETED`);
+  updateDb(db);
+  
+  res.status(200).json(db);
 });
 
+// Default route to main Note Taker page
 app.get('*', (req, res) => { 
   if (res.statusCode === '404') return;
   res.status(200).sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+// Server Listener
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)
 );
 
+// Update notes JSON
 function updateDb(db) {
   fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
     err ? console.log(new Error) : console.log('db.json updated!');
